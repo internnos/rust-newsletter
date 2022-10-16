1a162084f3bf4e0729c6dea254e375f7e98afc1f
internos <amajidsinar@gmail.com>
2022-10-17 00:25:49 +0700

    refactor(run): use listener as arg

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 use actix_web::{web, App, HttpServer, Responder, HttpResponse, dev::Server};
-
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run(address: &str) -> Result<Server, std::io::Error>{
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new(|| {
         App::new().route("/health_check", web::get().to(health_check))
     })
-    .bind(address)?
+    .listen(listener)?
     .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()>{
-    run("127.0.0.1:8000")?.await
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    run(listener)?.await
 }
