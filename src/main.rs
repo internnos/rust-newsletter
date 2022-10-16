898c3b66ac5e76eeb7602e372d56206e7cddb19a
internos <amajidsinar@gmail.com>
2022-10-17 01:51:10 +0700

    feat: change main port

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()>{
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
     run(listener)?.await
 }
